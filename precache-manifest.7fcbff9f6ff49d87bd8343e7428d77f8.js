c60e41a735c4203eeaa1f8f58bf7c0d66df2d5e4
Infi-Knight <soniravi829@gmail.com>
2020-11-04 18:38:38 +0530

    Updates

---
+++ precache-manifest.7fcbff9f6ff49d87bd8343e7428d77f8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7811b9379d980c1ee5a2d586ea49ac3",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a715d607b4e61d9e53c4",
+    "url": "./static/css/main.1b9bcb54.chunk.css"
+  },
+  {
+    "revision": "d6bc735f63c6e6b640c5",
+    "url": "./static/js/2.f4b06a17.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.f4b06a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a715d607b4e61d9e53c4",
+    "url": "./static/js/main.865a60be.chunk.js"
+  },
+  {
+    "revision": "d12705cc0642380537d9",
+    "url": "./static/js/runtime-main.47e82608.js"
+  }
+]);
