14b43431b4cff9df488dc8b6890ed5ed18f45fb2
Infi-Knight <soniravi829@gmail.com>
2020-11-05 03:56:24 +0530

    Updates

---
+++ precache-manifest.65f5dcf4280d65d1a4ed79e522955043.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbc0c8482767d2c62f867fae85b1bd3",
+    "url": "/line-chart-react/index.html"
+  },
+  {
+    "revision": "91fa18eaba7dcfc755d4",
+    "url": "/line-chart-react/static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "d6bc735f63c6e6b640c5",
+    "url": "/line-chart-react/static/js/2.f4b06a17.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/line-chart-react/static/js/2.f4b06a17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91fa18eaba7dcfc755d4",
+    "url": "/line-chart-react/static/js/main.5d2957f0.chunk.js"
+  },
+  {
+    "revision": "338e2f1704a97717d988",
+    "url": "/line-chart-react/static/js/runtime-main.e58ec5b8.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.119746bf5ee85cc301850d3d44c0565c.js"
+  "/line-chart-react/precache-manifest.65f5dcf4280d65d1a4ed79e522955043.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/line-chart-react/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
