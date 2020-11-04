8ff888c1eb6bc63d9052e757ba72b6fb77e3bf82
Infi-Knight <soniravi829@gmail.com>
2020-11-05 03:44:49 +0530

    Updates

--- precache-manifest.119746bf5ee85cc301850d3d44c0565c.js
+++ precache-manifest.119746bf5ee85cc301850d3d44c0565c.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0525e8a9b896f4354517003745669f11",
+    "revision": "920913e2de7a53a37cd2ce2455e15f2b",
     "url": "./index.html"
   },
   {
-    "revision": "812d39f2e1553ee1e289",
+    "revision": "91fa18eaba7dcfc755d4",
     "url": "./static/css/main.42cc8b22.chunk.css"
   },
   {
-    "revision": "a9551d2674a6e96ec0ee",
-    "url": "./static/js/2.3e500e3b.chunk.js"
+    "revision": "d6bc735f63c6e6b640c5",
+    "url": "./static/js/2.f4b06a17.chunk.js"
   },
   {
     "revision": "570d362d673dab785e62d2b8563e1118",
-    "url": "./static/js/2.3e500e3b.chunk.js.LICENSE.txt"
+    "url": "./static/js/2.f4b06a17.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "812d39f2e1553ee1e289",
-    "url": "./static/js/main.f6b0b482.chunk.js"
+    "revision": "91fa18eaba7dcfc755d4",
+    "url": "./static/js/main.5d2957f0.chunk.js"
   },
   {
     "revision": "d12705cc0642380537d9",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.43202f1432984073537553e2ec01caf4.js"
+  "./precache-manifest.119746bf5ee85cc301850d3d44c0565c.js"
 );
 
 self.addEventListener('message', (event) => {
